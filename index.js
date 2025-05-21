2cf37529f2254a5725f8eaef9b6370fbd868c310
QuangHung <quanghungbg2021@gmail.com>
2025-05-21 23:28:41 +0700

    udpate

--- index.js
+++ index.js
@@ -240,93 +240,125 @@ app.get("/:region_id/query0", setCurrentPool, async (req, res) => {
 app.get("/:region_id/query2", setCurrentPool, async (req, res) => {
   try {
     const pool = req.currentPool;
-    const data = pool
-      .request()
-      .query(
-        "SELECT t1.* FROM (SELECT 'EU' AS region, COUNT(*) AS num_orders FROM [LINK_EU].[LogisticDB].[dbo].[Orders] o ) AS t1 UNION ALL " +
-          "SELECT t2.* FROM (SELECT 'AS' AS region, COUNT(*) AS num_orders FROM [LINK_AS].[LogisticDB].[dbo].[Orders] o ) AS t2 UNION ALL " +
-          "SELECT t3.* FROM (SELECT 'AU' AS region, COUNT(*) AS num_orders FROM [LINK_AU].[LogisticDB].[dbo].[Orders] o ) AS t3 UNION ALL " +
-          "SELECT t4.* FROM (SELECT 'AF' AS region, COUNT(*) AS num_orders FROM [LINK_AF].[LogisticDB].[dbo].[Orders] o ) AS t4 "
-      );
 
-    console.log("data", data);
-    data.then((res1) => {
-      return res.json(res1);
-    });
+    const result = await pool.request().query(`
+      SELECT 'EU' AS region, COUNT(*) AS num_orders FROM [LINK_EU].[LogisticDB].[dbo].[Orders]
+      UNION ALL
+      SELECT 'AS' AS region, COUNT(*) AS num_orders FROM [LINK_AS].[LogisticDB].[dbo].[Orders]
+      UNION ALL
+      SELECT 'AU' AS region, COUNT(*) AS num_orders FROM [LINK_AU].[LogisticDB].[dbo].[Orders]
+      UNION ALL
+      SELECT 'AF' AS region, COUNT(*) AS num_orders FROM [LINK_AF].[LogisticDB].[dbo].[Orders]
+    `);
+
+    res.json(result); // hoặc res.json(result.recordset);
   } catch (err) {
-    console.log(err);
+    console.error("Query error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 //câu truy vấn 3: tổng giá trị hóa đơn (bao gồm cả thuế vat) của mỗi vùng
 app.get("/:region_id/query3", setCurrentPool, async (req, res) => {
   try {
     const pool = req.currentPool;
-    const data = pool
-      .request()
-      .query(
-        "SELECT t1.* FROM (SELECT 'EU' AS region, sum(o.amount+ i.vat) AS num_orders FROM [LINK_EU].[LogisticDB].[dbo].[Invoice] i JOIN [LINK_EU].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id) AS t1 UNION ALL " +
-          "SELECT t2.* FROM (SELECT 'AS' AS region, sum(o.amount+ i.vat) AS num_orders FROM [LINK_AS].[LogisticDB].[dbo].[Invoice] i JOIN [LINK_AS].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id) AS t2 UNION ALL " +
-          "SELECT t3.* FROM (SELECT 'AU' AS region, sum(o.amount+ i.vat) AS num_orders FROM [LINK_AU].[LogisticDB].[dbo].[Invoice] i JOIN [LINK_AU].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id) AS t3 UNION ALL " +
-          "SELECT t4.*FROM (SELECT 'AF' AS region, sum(o.amount+ i.vat) AS num_orders FROM [LINK_AF].[LogisticDB].[dbo].[Invoice] i JOIN [LINK_AF].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id) AS t4"
-      );
 
-    console.log("data", data);
-    data.then((res1) => {
-      return res.json(res1);
-    });
+    const result = await pool.request().query(`
+      SELECT 'EU' AS region, sum(o.amount+ i.vat) AS num_orders
+      FROM [LINK_EU].[LogisticDB].[dbo].[Invoice] i 
+      JOIN [LINK_EU].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id
+      UNION ALL
+      SELECT 'AS' AS region, sum(o.amount+ i.vat) AS num_orders
+      FROM [LINK_AS].[LogisticDB].[dbo].[Invoice] i 
+      JOIN [LINK_AS].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id
+      UNION ALL
+      SELECT 'AU' AS region, sum(o.amount+ i.vat) AS num_orders
+      FROM [LINK_AU].[LogisticDB].[dbo].[Invoice] i 
+      JOIN [LINK_AU].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id
+      UNION ALL
+      SELECT 'AF' AS region, sum(o.amount+ i.vat) AS num_orders
+      FROM [LINK_AF].[LogisticDB].[dbo].[Invoice] i 
+      JOIN [LINK_AF].[LogisticDB].[dbo].[Orders] o ON i.order_id = o.order_id
+    `);
+
+    res.json(result.recordset); // trả về dữ liệu chính xác
   } catch (err) {
-    console.log(err);
+    console.error("Query3 error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 //câu truy vấn 4: Trung bình thời gian xử lý hải quan mỗi vùng
 
 app.get("/:region_id/query4", setCurrentPool, async (req, res) => {
   try {
     const pool = req.currentPool;
-    const data = pool
-      .request()
-      .query(
-        "SELECT t1.* FROM (SELECT 'EU' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_EU].[LogisticDB].[dbo].[Customs] c ) AS t1" +
-          "UNION ALL" +
-          +"SELECT t2.* FROM (SELECT 'AS' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_AS].[LogisticDB].[dbo].[Customs] c ) AS t2" +
-          "UNION ALL" +
-          +"SELECT t3.* FROM (SELECT 'AU' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_AU].[LogisticDB].[dbo].[Customs] c ) AS t3" +
-          "UNION ALL" +
-          +"SELECT t4.* FROM (SELECT 'AF' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_AF].[LogisticDB].[dbo].[Customs] c ) AS t4"
-      );
 
-    console.log("data", data);
-    data.then((res1) => {
-      return res.json(res1);
-    });
+    const result = await pool.request().query(`
+      
+        SELECT 'EU' AS region, AVG(c.processing_time) AS avg_processing_time
+        FROM [LINK_EU].[LogisticDB].[dbo].[Customs] c
+      
+      UNION ALL
+      
+        SELECT 'AS' AS region, AVG(c.processing_time) AS avg_processing_time
+        FROM [LINK_AS].[LogisticDB].[dbo].[Customs] c
+      
+      UNION ALL
+      
+        SELECT 'AU' AS region, AVG(c.processing_time) AS avg_processing_time
+        FROM [LINK_AU].[LogisticDB].[dbo].[Customs] c
+      
+      UNION ALL
+      
+        SELECT 'AF' AS region, AVG(c.processing_time) AS avg_processing_time
+        FROM [LINK_AF].[LogisticDB].[dbo].[Customs] c
+      
+    `);
+
+    res.json(result.recordset);
   } catch (err) {
-    console.log(err);
+    console.error("Query4 error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-//câu truy vấn 5: Trung bình số lượng đơn hàng mỗi khách hàng theo vùng
 
 app.get("/:region_id/query5", setCurrentPool, async (req, res) => {
   try {
     const pool = req.currentPool;
-    const data = pool
-      .request()
-      .query(
-        "SELECT t1.* FROM (SELECT 'EU' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_EU].[LogisticDB].[dbo].[Customs] c ) AS t1" +
-          "UNION ALL" +
-          +"SELECT t2.* FROM (SELECT 'AS' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_AS].[LogisticDB].[dbo].[Customs] c ) AS t2" +
-          "UNION ALL" +
-          +"SELECT t3.* FROM (SELECT 'AU' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_AU].[LogisticDB].[dbo].[Customs] c ) AS t3" +
-          "UNION ALL" +
-          +"SELECT t4.* FROM (SELECT 'AF' AS region, avg(c.processing_time) AS avg_processing_time FROM [LINK_AF].[LogisticDB].[dbo].[Customs] c ) AS t4"
-      );
 
-    console.log("data", data);
-    data.then((res1) => {
-      return res.json(res1);
-    });
+    const query = `
+      SELECT 'EU' AS region, 
+		ROUND( CAST(COUNT(*) AS FLOAT) / (SELECT COUNT(*) 
+		FROM [LINK_EU].[LogisticDB].[dbo].[Customer] c
+		JOIN [LINK_EU].[LogisticDB].[dbo].[Country] co ON c.country_id = co.country_id
+		), 2) AS Ti_Le
+		FROM [LINK_EU].[LogisticDB].[dbo].[Orders]
+    UNION ALL
+    SELECT 'AS' AS region, 
+        ROUND( CAST(COUNT(*) AS FLOAT) / (SELECT COUNT(*) 
+        FROM [LINK_AS].[LogisticDB].[dbo].[Customer] c
+        JOIN [LINK_AS].[LogisticDB].[dbo].[Country] co ON c.country_id = co.country_id
+        ), 2) AS Ti_Le
+        FROM [LINK_AS].[LogisticDB].[dbo].[Orders]
+    UNION ALL
+    SELECT 'AU' AS region, 
+        ROUND( CAST(COUNT(*) AS FLOAT) / (SELECT COUNT(*) 
+        FROM [LINK_AU].[LogisticDB].[dbo].[Customer] c
+        JOIN [LINK_AU].[LogisticDB].[dbo].[Country] co ON c.country_id = co.country_id
+        ), 2) AS Ti_Le
+        FROM [LINK_AU].[LogisticDB].[dbo].[Orders]
+    UNION ALL
+    SELECT 'AF' AS region, ROUND( CAST(COUNT(*) AS FLOAT) / (SELECT COUNT(*) 
+    FROM [LINK_AF].[LogisticDB].[dbo].[Customer] c JOIN [LINK_AF].[LogisticDB].[dbo].[Country] co ON c.country_id = co.country_id), 2) AS Ti_Le
+    FROM [LINK_AF].[LogisticDB].[dbo].[Orders]
+    `;
+
+    const result = await pool.request().query(query);
+    res.json(result.recordset);
   } catch (err) {
-    console.log(err);
+    console.error("Query5 error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
